24cb1affdaa63f1b4ff7233db6a9a5728bfc8095
Pengkever <pengkever@gmail.com>
2018-03-04 11:43:33 +0800

    doubanFilm

---
+++ pages/detail/detail.js
@@ -0,0 +1,31 @@
+
+var api = require('../../utils/api.js');
+
+// pages/detail/detail.js
+Page({
+  data:{
+    list:{}
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+    var id = options.id;
+    api.getDetail(id)
+      .then(res=>{
+         this.setData({
+           list:res
+         });
+      })
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
---
+++ pages/home/home.js
@@ -0,0 +1,73 @@
+var api = require('../../utils/api.js');
+
+Page({
+  data:{
+    pn:0,
+    list:[],    
+    showMore:true,
+    showLoading:true
+  },
+  redirect:function(e){
+    var id = e.currentTarget.dataset.id;
+    wx.navigateTo({
+        url:'../detail/detail?id='+id
+    })
+  },
+  scrolltolower:function(e){
+    if(!this.data.showMore)return;
+    this.loadData(this.data.pn);
+  },
+  loadData:function(pn){
+    api.getList('in_theaters',pn)
+        .then(res=>{
+            if(res.subjects.length>0){
+                this.setData({
+                  list:this.data.list.concat(res.subjects),
+                  showLoading:false,
+                  pn:pn+1
+                })
+            }
+            else{
+              this.setData({
+                showMore:false
+              })
+            }
+        })
+  },
+  onLoad:function(options){
+    // 生命周期函数--监听页面加载
+     this.loadData(this.data.pn)
+  },
+  onReady:function(){
+    // 生命周期函数--监听页面初次渲染完成
+   
+  },
+  onShow:function(){
+    // 生命周期函数--监听页面显示
+   
+  },
+  onHide:function(){
+    // 生命周期函数--监听页面隐藏
+   
+  },
+  onUnload:function(){
+    // 生命周期函数--监听页面卸载
+    
+  },
+  onPullDownRefresh: function() {
+    // 页面相关事件处理函数--监听用户下拉动作
+    
+  },
+  onReachBottom: function() {
+    // 页面上拉触底事件的处理函数
+   
+  },
+  onShareAppMessage: function() {
+    // 用户点击右上角分享
+    return {
+      title: 'title', // 分享标题
+      desc: 'desc', // 分享描述
+      path: 'path' // 分享路径
+    }
+  }
+})
